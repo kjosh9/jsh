0454d63876710b737dfa0ef2c6fbec14f24f3b07
Joshua Knestautx <jaknestaut@gmail.com>
2021-03-14 09:56:33 -0400

    Add ability to execute simple programs with no params

--- jsh.c
+++ jsh.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+char * parse_input(char * input)
+{
+    return strtok(input, " "); 
+}
 
 int main(void)
 {
-    char input_string[200];
+    char input_str[256];
     while (true) {
-        printf("[jsh]");
-        gets(input_str);
+        printf("[jsh]-->");
+        fgets(input_str, sizeof(input_str), stdin);
+        char * command = parse_input(input_str);
+        
+        if (fork() == 0) {
+            command = "ls";
+            char *args[] = {command, NULL};
+            execvp(command, args);
+        } else {
+            wait(NULL);
+        }
     }
     return 0;
 }
