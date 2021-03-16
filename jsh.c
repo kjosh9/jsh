bc06718ec879be6145c24ef99ed07c65db4ee653
Joshua Knestautx <jaknestaut@gmail.com>
2021-03-15 23:51:52 -0400

    Add simple history
    Add feature is input "!!" runs previous command

--- jsh.c
+++ jsh.c
@@ -9,6 +9,12 @@
 #define MAX_INPUT_LEN 256
 #define MAX_INPUT_TOKENS 10
 
+struct commandNode {
+    char* command;
+    struct commandNode* prev;
+    struct commandNode* next;
+};
+
 char** parse_input(char * input)
 {
     char** tokens = malloc(sizeof(char*) * MAX_INPUT_TOKENS);
@@ -26,10 +32,25 @@ char** parse_input(char * input)
     return tokens;
 }
 
+void execute_command(char ** command)
+{
+    if (fork() == 0) {
+        execvp(command[0], command);
+    } else {
+        wait(NULL);
+    }
+}
+
 int main(void)
 {
     char input_str[256];
     bool exit_called = false;
+    struct commandNode * head;
+    head = malloc(sizeof *head);
+    head->prev = NULL;
+    head->next = NULL;
+    struct commandNode * curr = head;
+
     while (!exit_called) {
 
         printf("[jsh]-->");
@@ -39,20 +60,42 @@ int main(void)
         char* p;
         if ((p = strchr(input_str, '\n')) != NULL)
             *p = '\0';
-
+        curr->command = strdup(input_str);
         char** command = parse_input(input_str);
         
         if (!strcmp(command[0], "exit")) {
             exit_called = true;
-        } else if (fork() == 0) {
-            execvp(command[0], command);
         } else {
-            wait(NULL);
+ 
+            if (!strcmp(command[0], "!!")) {
+                for (int i=0; i<MAX_INPUT_TOKENS; i++) {
+                    free(command[i]);
+                }
+                free(command);
+                command = parse_input(curr->prev->command);
+            }
+
+            execute_command(command);
         }
         for (int i=0; i<MAX_INPUT_TOKENS; i++) {
             free(command[i]);
         }
         free(command);
+
+        if(!exit_called) {
+            struct commandNode * next = malloc(sizeof *head);
+            curr->next = next;
+            next->prev = curr;
+            next->next = NULL;
+            curr = next;
+        }
+    }
+
+    while(head != NULL) {
+        struct commandNode * temp = head->next;
+        free(head->command);
+        free(head);
+        head = temp;
     }
     return 0;
 }
